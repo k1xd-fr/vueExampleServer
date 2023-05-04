41f630c08f4673661ba23b545163f48ef751a409
k1xd-fr <117137914+k1xd-fr@users.noreply.github.com>
2023-05-04 20:48:10 +0600

    add

---
+++ build/transfer-tokens-edit-page.ee3909fd.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkserver=self.webpackChunkserver||[]).push([[2492],{9452:(P,s,e)=>{e.r(s),e.d(s,{default:()=>_});var t=e(67294),n=e(95489),a=e.n(n),r=e(87751),E=e(53219);const _=()=>t.createElement(n.CheckPagePermissions,{permissions:r.Z.settings["transfer-tokens"].read},t.createElement(E.Z,null))}}]);
